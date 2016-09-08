1fb13a19ae0209c34ac2f8ede5dd7afed6e44d8c
Luc DUZAN <lduzan@linagora.com>
2016-09-08 19:05:44 +0200

    CHAT-78 add method needed for moderation

--- backend/lib/conversation.js
+++ backend/lib/conversation.js
@@ -329,6 +329,18 @@ module.exports = function(dependencies) {
     });
   }
 
+  function moderateConversation(conversationId, moderate, callback) {
+    Conversation.findByIdAndUpdate(conversationId, {
+      $set: {moderate: moderate}
+    }, callback);
+  }
+  
+  function moderateMessage(messageId, moderate, callback) {
+    ChatMessage.findByIdAndUpdate(messageId, {
+      $set: {moderate: moderate}
+    }, callback);
+  }
+
   function removeMemberFromConversation(conversationId, userId, callback) {
     var unsetOperation = {};
     unsetOperation['numOfReadedMessage.' + userId] = '';
@@ -408,6 +420,8 @@ module.exports = function(dependencies) {
     addMemberToConversation: addMemberToConversation,
     updateCommunityConversation: updateCommunityConversation,
     updateConversation: updateConversation,
+    moderateConversation: moderateConversation,
+    moderateMessage: moderateMessage,
     removeMemberFromConversation: removeMemberFromConversation,
     findConversation: findConversation,
     createMessage: createMessage,
