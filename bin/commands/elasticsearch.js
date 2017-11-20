a94aa8c64e13f569b9b9c27cf6cf60fe56ff817d
abenhariz <abenhariz@linagora.com>
2017-11-20 15:13:24 +0100

    #452 update the ./bin/cli elasticsearch

--- bin/commands/elasticsearch.js
+++ bin/commands/elasticsearch.js
@@ -2,16 +2,17 @@
 
 const path = require('path');
 const ESConfiguration = require('esn-elasticsearch-configuration');
-const INDEXES = ['chat.messages', 'chat.conversations'];
+const INDEX_TYPES = ['chat.messages', 'chat.conversations'];
 
 function createCommand(command) {
   command
     .description('Configure ElasticSearch for the chat module')
     .option('-h, --host <host>', 'elasticsearch host to connect to')
     .option('-p, --port <port>', 'elasticsearch port to connect to')
+    .option('-t, --type <type>', 'type of index')
     .option('-i, --index <index>', 'index to create')
     .action(cmd => {
-      exec(cmd.host, cmd.port, cmd.index).then(() => {
+      exec(cmd.host, cmd.port, cmd.type, cmd.index).then(() => {
         console.log('ElasticSearch has been configured');
       }, err => {
         console.log('Error while configuring ElasticSearch', err);
@@ -22,15 +23,20 @@ function createCommand(command) {
     });
 }
 
-function exec(host = 'localhost', port = 9200, index) {
-  const p = path.normalize(__dirname + '/../../config/elasticsearch/');
-  const conf = new ESConfiguration({path: p, host, port});
+function exec(host = 'localhost', port = 9200, type, index) {
+  const conf = new ESConfiguration({host: host, port: port});
 
-  if (index) {
-    return conf.createIndex(index);
+  if (type) {
+    index = index || _getDefaultIndex(type);
+
+    return conf.setup(index, type);
   }
 
-  return Promise.all(INDEXES.map(index => conf.createIndex(index)));
+  return Promise.all(INDEX_TYPES.map(type => conf.setup(_getDefaultIndex(type), type)));
+}
+
+function _getDefaultIndex(type) {
+  return `${type}.idx`;
 }
 
 module.exports.createCommand = createCommand;
