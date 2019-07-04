9e01c3461288db793a9ff889047df4ae19c4904e
Loic AUBLET <laublet@linagora.com>
2019-07-04 14:37:04 +0200

    esn#2706 use the objectType to disable chatLaunchConversation button if needed

--- frontend/app/components/launch-conversation-button/launch-conversation-button.controller.js
+++ frontend/app/components/launch-conversation-button/launch-conversation-button.controller.js
@@ -6,6 +6,16 @@
 
   function chatLaunchConversationButtonController(session) {
     var self = this;
-    self.isCurrentUser = self.userId === session.user._id;
+
+    self.isNotUser = isNotUser;
+
+    if (!self.isCurrentUser) {
+      self.isCurrentUser = self.userId === session.user._id;
+    }
+
+    function isNotUser() {
+      return !!(self.isCurrentUser || self.objectType !== 'user');
+    }
+
   }
 })();
